836dbacc17e45fb010751cf8ac3a96fe25d99920
xN8Tx <theeldern8t@ya.ru>
2025-02-23 01:47:28 +0700

    fix: change CORS rules in strapi

--- app/strapi/config/middlewares.ts
+++ app/strapi/config/middlewares.ts
@@ -2,7 +2,16 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://pill.shteyn-web.ru"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["*"],
+      keepHeaderOnError: true,
+      credentials: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
