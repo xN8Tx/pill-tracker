4f984044bbdf67d76eb24eede790bed903e6d529
xN8Tx <theeldern8t@ya.ru>
2025-02-23 00:31:49 +0700

    feat: add url to server config strapi

--- app/strapi/config/server.ts
+++ app/strapi/config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL", "https://pill-cms.shteyn-web.ru"),
+  proxy: true,
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
